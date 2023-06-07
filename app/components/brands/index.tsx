65f9f401190f3d147323299df6b4b1acaea44501
Rafael Grande <rafael@grande.com>
2023-06-07 19:09:40 -0300

    Removendo scroll no mobile

--- app/components/brands/index.tsx
+++ app/components/brands/index.tsx
@@ -1,11 +1,11 @@
 const Brands = () => (
     <section className="py-2 relative border-b border-b-orange-400 bg-orange-400 text-white overflow-hidden">
-        <div className="overflow-auto hover:overflow-scroll">
-            <div className="flex gap-2 text-sm font-bold divide-x">
-                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0">Fiat</div>
-                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0">Ford</div>
-                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0">Ford</div>
-                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0">Ford</div>
+        <div className="noscroll overflow-scroll snap-x touch-pan-x snap-center">
+            <div className="flex gap-2 text-sm font-bold divide-x snap-x">
+                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center">Fiat</div>
+                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center">Ford</div>
+                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center">Ford</div>
+                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center">Ford</div>
             </div>
         </div>
     </section>
