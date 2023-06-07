d7271aabdca6532d32df0797b76fb3d7950e0d46
Rafael Grande <rafael@grande.com>
2023-06-07 19:33:55 -0300

    Ajustes finais no menu mobile

--- app/components/brands/index.tsx
+++ app/components/brands/index.tsx
@@ -1,11 +1,13 @@
 const Brands = () => (
-    <section className="py-2 relative border-b border-b-orange-400 bg-orange-400 text-white overflow-hidden">
+    <section className="py-2 relative border-b border-b-orange-400 text-white overflow-hidden">
         <div className="noscroll overflow-scroll snap-x touch-pan-x snap-center">
             <div className="flex gap-2 text-sm font-bold divide-x snap-x">
-                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center">Fiat</div>
-                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center">Ford</div>
-                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center">Ford</div>
-                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center">Ford</div>
+                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center bg-orange-400 rounded-md first:ml-2 last:mr-2">Fiat</div>
+                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center bg-orange-400 rounded-md first:ml-2 last:mr-2">Ford</div>
+                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center bg-orange-400 rounded-md first:ml-2 last:mr-2">Volkswagem</div>
+                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center bg-orange-400 rounded-md first:ml-2 last:mr-2">Audi</div>
+                <div className="p-2 w-4/12 text-center basis-1/3 shrink-0 snap-center bg-orange-400 rounded-md first:ml-2 last:mr-2">Huyndai</div>
+
             </div>
         </div>
     </section>
