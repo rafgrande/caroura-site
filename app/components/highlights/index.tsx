6b3f904b5fe683f82d9e26d2013b1e0137e84fd3
Rafael Grande <rafael@grande.com>
2023-06-09 02:40:10 -0300

    Ajuste no touch nos sliders

--- app/components/highlights/index.tsx
+++ app/components/highlights/index.tsx
@@ -1,17 +1,17 @@
 const Highlights = () => (
     <>
         <section className='py-2 relative border-b border-b-orange-400 overflow-hidden lg:max-w-3xl lg:m-auto'>
-            <div className="noscroll overflow-scroll snap-x touch-pan-x snap-center flex text-sm font-bold lg:justify-center gap-2">
-                <div className="border-orange-400 border-2 p-1 rounded-full basis-24 h-24 shrink-0">
+            <div className="noscroll overflow-scroll snap-x flex text-sm font-bold lg:justify-center gap-2">
+                <div className="border-orange-400 border-2 p-1 rounded-full basis-24 h-24 shrink-0 snap-center">
                     <div className="flex justify-center items-center rounded-full bg-orange-400 p-2 text-center text-white w-full h-full">Gol</div>
                 </div>
-                <div className="border-orange-400 border-2 p-1 rounded-full basis-24 h-24 shrink-0">
+                <div className="border-orange-400 border-2 p-1 rounded-full basis-24 h-24 shrink-0 snap-center">
                     <div className="flex justify-center items-center rounded-full bg-orange-400 p-2 text-center text-white  w-full h-full">Gol</div>
                 </div>
-                <div className="border-orange-400 border-2 p-1 rounded-full basis-24 h-24 shrink-0">
+                <div className="border-orange-400 border-2 p-1 rounded-full basis-24 h-24 shrink-0 snap-center">
                     <div className="flex justify-center items-center rounded-full bg-orange-400 p-2 text-center text-white  w-full h-full">Gol</div>
                 </div>
-                <div className="border-orange-400 border-2 p-1 rounded-full basis-24 h-24 shrink-0">
+                <div className="border-orange-400 border-2 p-1 rounded-full basis-24 h-24 shrink-0 snap-center">
                     <div className="flex justify-center items-center rounded-full bg-orange-400 p-2 text-center text-white  w-full h-full">Gol</div>
                 </div>
             </div>
