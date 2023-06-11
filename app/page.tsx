647693dd59dba48015871a3f678d9d5bff399100
Rafael Grande <rafael@grande.com>
2023-06-11 04:35:45 -0300

    Criando o componente do banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 import Brands from "./components/brands";
 import Filter from "./components/filter";
 import Highlights from "./components/highlights"
 import Showcase from "./components/showcase";
-
+import banner from './components/img/banner.png';
 
 export default function Home() {
   return (
@@ -11,7 +12,10 @@ export default function Home() {
         <Brands />
         <Showcase />
         <Highlights/>
-        <Showcase />        
+        <Showcase />  
+        <div className="p-2 h-14 relative">
+          <Image src={banner} alt='banner' fill={true} />
+        </div>     
     </main>
   )
 }
